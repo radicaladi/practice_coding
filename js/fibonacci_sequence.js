c590e8bd81f9edbb3032e95f22dfe8fbb4e80e5c
Adrian Brown <adrian.r.brown90@gmail.com>
2021-12-07 14:03:26 -0700

    created fibonacci sequence function

---
+++ js/fibonacci_sequence.js
@@ -0,0 +1,20 @@
+"use strict";
+
+// A function to produce the fibonacci sequence up to the terms declared by the user.
+
+function fibonacciSequence (terms) {
+    const number = terms; // terms is the number of terms that will be expressde by the fibonacci sequence.
+    let n1 = 0, n2 = 1, nextTerm; // variables that will determine the math output of the fibonacci sequence.
+    console.log("fibonacci sequence: ");
+    // using a for loop as the number of terms to cycle through is known and declared to the function.
+    for (let i = 0; i <= number ; i++) {  //won't exceed the number of loops chosen by terms.
+        console.log(n1);
+        nextTerm = n1 + n2;
+        n1 = n2;
+        n2 = nextTerm;
+        // fibonacci sequence is a + b returning c and adding b... so on and so forth. Starting at zero.
+    }
+}
+
+fibonacciSequence(20);
+//enter an amount of terms and the function will output the sequence that number of times
