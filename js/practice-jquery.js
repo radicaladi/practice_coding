78deade9bd0879dd6bb352b2596996af70261321
Adrian Brown <adrian.r.brown90@gmail.com>
2022-01-21 10:08:49 -0700

    added relevant jquery, js links, and first three solutions

--- js/practice-jquery.js
+++ js/practice-jquery.js
@@ -1,13 +1,20 @@
 //mini - exercise:
 // using jquery:
 // 1. give all table cells a black border
+$('th, tr, td').css("border", "1px solid black");
+
 // 2. remove all bullet points from the unordered list items
+$('li').css("list-style", "none");
+
 // 3. change the font family of the entire page to luxurious roman
+$('body').css("font-family", "luxurious roman, cursive");
+
 //    ---> <link rel=“preconnect” href=“https://fonts.googleapis.com”>
 //         <link rel=“preconnect” href=“https://fonts.gstatic.com” crossorigin>
 //         <link href=“https://fonts.googleapis.com/css2?family=Abril+Fatface&family=Questrial&display=swap” rel=“stylesheet”>
 //    --->font-family: ‘Abril Fatface’, cursive;
 //        font-family: ‘Questrial’, sans-serif;
+
 // 4. give each section a unique id
 // 5. using the unique id’s give each section a different background color
 // 6. give 3 elements the same class name of “highlight”
