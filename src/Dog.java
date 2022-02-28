b167ac549a1d0b95b65f974712945ffe7c2d158a
Adrian Brown <adrian.r.brown90@gmail.com>
2022-02-28 11:11:48 -0700

    add dog function speak; oop practice

---
+++ src/Dog.java
@@ -0,0 +1,12 @@
+public class Dog {
+    public void speak()
+    {
+        System.out.println("woof!");
+    }
+
+    public static void main(String[] args)
+    {
+        Dog d = new Dog();
+        d.speak();
+    }
+}
