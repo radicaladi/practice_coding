d400950748d868dc0a4b108d8b931a4724501006
Adrian Brown <adrian.r.brown90@gmail.com>
2022-02-28 13:27:45 -0700

    add basic practice of java arrays with comments

--- src/Array.java
+++ src/Array.java
@@ -1,6 +1,6 @@
 import java.util.Arrays;
 
-public class Arrays {
+public class Array {
     public static void main(String[] args) {
 
         // Java Arrays requirements:
@@ -24,8 +24,14 @@ public class Arrays {
 
 
         // array that holds daily steps
-
-        long[] dailySteps = {10000, 12000, 22000};
+        int[] dailySteps = {10000, 12000, 22000};
         System.out.println(Arrays.toString(dailySteps));
+
+        // an array of ints and print out the array
+        int[] customers = {100, 50, 160};
+        System.out.println(Arrays.toString(customers));
+
+        // create a method (nums) that returns the sum of all integers in an int array
+        // [5, 2, 3, 1] // 11
     }
 }
