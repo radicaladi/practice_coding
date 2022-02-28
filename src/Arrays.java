911dc011222ffc5617edf6b2042a9d751d3cde0e
Adrian Brown <adrian.r.brown90@gmail.com>
2022-02-28 13:05:31 -0700

    initial commit; add roadmap to create java arrays

---
+++ src/Arrays.java
@@ -0,0 +1,12 @@
+public class Arrays {
+    public static void main(String[] args) {
+
+        // step 1: create variable of array type.
+        // String[] name; int[] name; etc
+        // step 2: assign same typing as a new array (right side)
+        // new String[]; new int[]; etc
+        // step 3: assign length of array; (number inside brackets)
+        // [3]; [5]; etc
+
+    }
+}
