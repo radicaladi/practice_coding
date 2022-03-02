bb8edfca602c4256e2bb098eef7b5964f375bafc
Adrian Brown <adrian.r.brown90@gmail.com>
2022-03-02 09:48:21 -0700

    adds new inface calls from objects

--- src/interfaces_abstracts/Company.java
+++ src/interfaces_abstracts/Company.java
@@ -8,11 +8,27 @@ public class Company {
         Employee cleanerMan = new Custodian("CleanerMan", "Maintenance");
 
         // calling their work method
-        System.out.println("NumberMan at work: " + numberMan.work());
-        System.out.println("NumberMan name: " + numberMan.getName());
+        System.out.println("What's your name and department? \n\t" + numberMan.getName() + " | " + numberMan.getDepartment());
+        System.out.println("NumberMan starting work day: \n\t" + numberMan.morningMeeting());
+        System.out.println("NumberMan at work: \n\t" + numberMan.work());
+        System.out.println("NumberMan at lunch: \n\t" + numberMan.lunchTime());
+        System.out.println("NumberMan day wage: \n\t$" + numberMan.dailyPay());
+        System.out.println("NumberMan on pay day: \n\t" + numberMan.getPaid());
+//        System.out.println("NumberMan name: " + numberMan.getName());
+        System.out.println();
 
-        // getting their name
-        System.out.println("CleanerMan at work: " + cleanerMan.work());
-        System.out.println("CleanerMan name: " + cleanerMan.getName());
+        System.out.println("What's your name and department? \n\t" + cleanerMan.getName() + " | " + cleanerMan.getDepartment());
+        System.out.println("CleanerMan starting work day: \n\t" + cleanerMan.morningMeeting());
+        System.out.println("CleanerMan at work: \n\t" + cleanerMan.work());
+        System.out.println("CleanerMan at lunch: \n\t" + cleanerMan.lunchTime());
+        System.out.println("CleanerMan day wage: \n\t$" + cleanerMan.dailyPay());
+        System.out.println("CleanerMan on pay day: \n\t" + cleanerMan.getPaid());
+//        System.out.println("CleanerMan name: " + cleanerMan.getName());
+        System.out.println();
+
+
+
+
+
     }
 }
