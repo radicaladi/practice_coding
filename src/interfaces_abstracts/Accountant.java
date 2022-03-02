3adaf1a86633f1349502abea84a28f85692e9544
Adrian Brown <adrian.r.brown90@gmail.com>
2022-03-02 09:47:24 -0700

    implements interface methods

--- src/interfaces_abstracts/Accountant.java
+++ src/interfaces_abstracts/Accountant.java
@@ -11,4 +11,22 @@ public class Accountant extends Employee {
     public String work() {
         return "TPS reports drafted!";
     }
+
+    // implemented methods,
+    public String morningMeeting() {
+        return "Accountants have morning meetings between 8:15AM and 8:45AM!";
+    }
+
+    public String lunchTime() {
+        return "Accountants take their lunch from 1:00PM - 2:00PM or as requested and approved through the accounting manager!";
+    }
+
+    public int dailyPay() {
+        return 500;
+    }
+
+    // monthlyWork method
+    public String getPaid() {
+        return "Arrived and received monthly wages!";
+    }
 }
--- src/interfaces_abstracts/Custodian.java
+++ src/interfaces_abstracts/Custodian.java
@@ -10,4 +10,22 @@ public class Custodian extends Employee {
     public String work(){
         return "Maintaining the building!";
     }
+
+    // implemented methods,
+    public String morningMeeting() {
+        return "Custodians have a direct report to the head of the maintenance department and schedule meetings as necessary!";
+    }
+
+    public String lunchTime() {
+        return "12:00PM - 1:00PM in the cafeteria!";
+    }
+
+    public int dailyPay() {
+        return 120;
+    }
+
+    // monthlyWork method
+    public String getPaid() {
+        return "Arrived and received monthly wages!";
+    }
 }
