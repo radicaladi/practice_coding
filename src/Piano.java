98d685ce15950e895bab1ce708266459790bb92a
Adrian Brown <adrian.r.brown90@gmail.com>
2022-03-07 13:29:32 -0700

    add constructor, getter, setter, sout messages of methods

--- src/Piano.java
+++ src/Piano.java
@@ -1,31 +1,46 @@
 public class Piano extends MusicalInstrument implements Tuneable, VolumeAdjustable {
+    private int numberOfKeys;
+
+    public Piano(String category, int numberOfKeys) {
+        super(category);
+        this.numberOfKeys = numberOfKeys;
+    }
+
+    public int getNumberOfKeys() {
+        return numberOfKeys;
+    }
+
+    public void setNumberOfKeys(int numberOfKeys) {
+        this.numberOfKeys = numberOfKeys;
+    }
+
     @Override
     public void startMusic() {
-
+        System.out.println("Piano starts to play...");
     }
 
     @Override
     public void stopMustic() {
-
+        System.out.println("Piano play comes to an end...");
     }
 
     @Override
     public void tuneInstrument() {
-
+        System.out.println("Piano wires adjusting...");
     }
 
     @Override
     public void detuneInstrument() {
-
+        System.out.println("Piano wires de-adjusting...");
     }
 
     @Override
     public void volumeUp() {
-
+        System.out.println("Piano noises intensifies...");
     }
 
     @Override
     public void volumeDown() {
-
+        System.out.println("Piano noises lessens...");
     }
 }
--- src/Sax.java
+++ src/Sax.java
@@ -1,31 +1,46 @@
 public class Sax extends MusicalInstrument implements Tuneable, VolumeAdjustable {
+    private int saxType;
+
+    public Sax(String category, int saxType){
+        super(category);
+        this.saxType = saxType;
+    }
+
+    public int getSaxType() {
+        return saxType;
+    }
+
+    public void setSaxType(int saxType) {
+        this.saxType = saxType;
+    }
+
     @Override
     public void startMusic() {
-
+        System.out.println("a harmonic Sax tune plays...");
     }
 
     @Override
     public void stopMustic() {
-
+        System.out.println("Sax play winds to an end...");
     }
 
     @Override
     public void tuneInstrument() {
-
+        System.out.println("Sax pitch adjusting...");
     }
 
     @Override
     public void detuneInstrument() {
-
+        System.out.println("Sax pitch re-adjusting...");
     }
 
     @Override
     public void volumeUp() {
-
+        System.out.println("Sax noises intensifies...");
     }
 
     @Override
     public void volumeDown() {
-
+        System.out.println("Sax noises lessens...");
     }
 }
