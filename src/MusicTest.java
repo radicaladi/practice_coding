8ea9e87ae51a2878fd96b3cd048782a488256b61
Adrian Brown <adrian.r.brown90@gmail.com>
2022-03-07 14:02:49 -0700

    instantiate piano and sax objects; add MusicTest local methods; test calls to classes; complete

--- src/MusicTest.java
+++ src/MusicTest.java
@@ -1,2 +1,26 @@
-package PACKAGE_NAME;public class MusicTest {
+public class MusicTest {
+    static MusicalInstrument Piano = new Piano("cordophone", 88);
+    static MusicalInstrument Sax = new Sax("woodwind", 4);
+
+    public static void detuneRetune (Tuneable tuneableObj) {
+        tuneableObj.detuneInstrument();
+        tuneableObj.tuneInstrument();
+    }
+
+    public static void makeTwoStaccatoSounds(VolumeAdjustable va) {
+        va.volumeUp();
+        va.volumeDown();
+        va.volumeUp();
+        va.volumeDown();
+    }
+
+    public static void main(String[] args) {
+        detuneRetune((Tuneable) Piano); // call Piano tuning method
+        System.out.println();
+        makeTwoStaccatoSounds((VolumeAdjustable) Piano); // call Piano volume methods
+        System.out.println();
+        detuneRetune((Tuneable) Sax); // call Sax tuning method
+        System.out.println();
+        makeTwoStaccatoSounds((VolumeAdjustable) Sax); // call Sax volume methods
+    }
 }
