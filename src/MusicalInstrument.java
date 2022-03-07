74c60541b7cfbcbe37be4f0bad81148ac7db0ff8
Adrian Brown <adrian.r.brown90@gmail.com>
2022-03-07 13:05:49 -0700

    add constructur, getter, setter

--- src/MusicalInstrument.java
+++ src/MusicalInstrument.java
@@ -1,3 +1,15 @@
 public abstract class MusicalInstrument implements Playable {
     private String instrumentClassification;
+
+    public MusicalInstrument(String category) {
+        this.instrumentClassification = category;
+    }
+
+    public String getInstrumentClassification() {
+        return instrumentClassification;
+    }
+
+    public void setInstrumentClassification(String instrumentClassification) {
+        this.instrumentClassification = instrumentClassification;
+    }
 }
