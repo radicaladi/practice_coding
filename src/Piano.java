d564f352e0f595d0f401fa1dba2afa414f9ce138
Adrian Brown <adrian.r.brown90@gmail.com>
2022-03-07 13:12:22 -0700

    initial commit

---
+++ src/Piano.java
@@ -0,0 +1,31 @@
+public class Piano extends MusicalInstrument implements Tuneable, VolumeAdjustable {
+    @Override
+    public void startMusic() {
+
+    }
+
+    @Override
+    public void stopMustic() {
+
+    }
+
+    @Override
+    public void tuneInstrument() {
+
+    }
+
+    @Override
+    public void detuneInstrument() {
+
+    }
+
+    @Override
+    public void volumeUp() {
+
+    }
+
+    @Override
+    public void volumeDown() {
+
+    }
+}
---
+++ src/Sax.java
@@ -0,0 +1,31 @@
+public class Sax extends MusicalInstrument implements Tuneable, VolumeAdjustable {
+    @Override
+    public void startMusic() {
+
+    }
+
+    @Override
+    public void stopMustic() {
+
+    }
+
+    @Override
+    public void tuneInstrument() {
+
+    }
+
+    @Override
+    public void detuneInstrument() {
+
+    }
+
+    @Override
+    public void volumeUp() {
+
+    }
+
+    @Override
+    public void volumeDown() {
+
+    }
+}
