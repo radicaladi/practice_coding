b65831b66c3ac8cccb01ee41ab4823b227e803d2
Adrian Brown <adrian.r.brown90@gmail.com>
2022-03-07 16:07:27 -0700

    minor changes

--- src/menu_calculator/Item.java
+++ src/menu_calculator/Item.java
@@ -1,33 +1,41 @@
 package menu_calculator;
 
-import java.util.ArrayList;
-
 public class Item {
     public String name;
-    public ArrayList<Double> prices;
+    public double prices;
 
-    public Item(String name){
+    public Item(String name, double price){
         this.name = name;
-        this.prices = new ArrayList<>();
+        this.prices = price;
     }
 
     public String getItemName(){
         return name;
     }
 
-    public void addPrice(double price) {
-        prices.add((price));
+    public void setItemName(String name) {
+        this.name = name;
     }
 
-    public ArrayList<Double> getItemPrice() {
+    public double getItemPrice(){
         return prices;
     }
 
+    public void setItemPrice(double price){
+        this.prices = price;
+    }
+
+
     public static void main(String[] args) {
-        Item ChickenStrips = new Item("Chicken Strips");
+        Item ChickenStrips = new Item("Chicken Strips", 3.50);
         System.out.println(ChickenStrips.getItemName());
-        ChickenStrips.addPrice(3.50);
         System.out.println(ChickenStrips.getItemPrice());
+        System.out.println("//////////");
+        Item FrenchFries = new Item("French Frys", 2);
+        FrenchFries.setItemName("French Fries");
+        FrenchFries.setItemPrice(2.50);
+        System.out.println(FrenchFries.getItemName());
+        System.out.println(FrenchFries.getItemPrice());
     }
 
 }
