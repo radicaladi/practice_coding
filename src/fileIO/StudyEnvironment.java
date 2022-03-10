557da05f2ea8a332a04daa21989f4b27c2eb5140
Adrian Brown <adrian.r.brown90@gmail.com>
2022-03-10 09:14:14 -0700

    add example of fileIO uses

--- src/fileIO/StudyEnvironment.java
+++ src/fileIO/StudyEnvironment.java
@@ -3,6 +3,7 @@ package fileIO;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.List;
 
 public class StudyEnvironment {
@@ -13,4 +14,17 @@ public class StudyEnvironment {
             System.out.printf("%d: %s\n", i + 1, fileContents.get(i));
         }
     }
+
+    public static void main(String[] args) throws IOException {
+        String myString = "This is a string";
+
+        Path poemToPath = Paths.get("src/fileIO/wcw.txt");
+
+        System.out.println("path to poem = " + poemToPath);
+
+        System.out.println("Files.exist(poemToPath) = " + Files.exists(poemToPath));
+
+        printPoem(poemToPath);
+    }
+
 }
