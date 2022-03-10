eb7323778de8f582aa332f56ebac2012d87c1f5d
Adrian Brown <adrian.r.brown90@gmail.com>
2022-03-10 09:21:59 -0700

    add comments

--- src/fileIO/StudyEnvironment.java
+++ src/fileIO/StudyEnvironment.java
@@ -16,14 +16,19 @@ public class StudyEnvironment {
     }
 
     public static void main(String[] args) throws IOException {
+        // the basic way of creating a string
         String myString = "This is a string";
 
+        // using paths.get to get a file and store it in a Path variable
         Path poemToPath = Paths.get("src/fileIO/wcw.txt");
 
+        // printing to console the content of the variable (the path)
         System.out.println("path to poem = " + poemToPath);
 
+        // printing to the console whether the file exists
         System.out.println("Files.exist(poemToPath) = " + Files.exists(poemToPath));
 
+        // print the file (poem) if it exists
         printPoem(poemToPath);
     }
 
