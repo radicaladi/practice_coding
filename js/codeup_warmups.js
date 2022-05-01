c98b7567697b30c229fa47cb20d4346171a60d15
Adrian Brown <adrian.r.brown90@gmail.com>
2022-04-30 20:00:49 -0700

    add conversion of java leet code to javascript function

--- js/codeup_warmups.js
+++ js/codeup_warmups.js
@@ -126,3 +126,11 @@ function checkPalindrome(string) {
 }
 
 checkPalindrome("bob")
+
+// Given two temperatures , return true if one is less than 0 and the other is greater than 100
+
+function icyHot(temp1, temp2) {
+    return temp1 < 0 && temp2 > 100 || temp1 > 100 && temp2 < 0;
+}
+
+console.log(icyHot(-2, 109))
