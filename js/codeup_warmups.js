a3ef4fae19b7e25433b5ca05eed9bc42c39cc205
Adrian Brown <adrian.r.brown90@gmail.com>
2022-05-01 18:35:08 -0700

    add leetcode function front22, convert to javascript from java

--- js/codeup_warmups.js
+++ js/codeup_warmups.js
@@ -134,3 +134,18 @@ function icyHot(temp1, temp2) {
 }
 
 console.log(icyHot(-2, 109))
+
+//Given a string, take the first 2 chars and return the string with the 2 chars added at both the front and back,
+// so "kitten" yields "kikittenki".
+// If the string length is less than 2, use whatever chars are there
+
+function front22(string) {
+    let take = 2;
+    if (take > string.length) {
+        take = string.length;
+    }
+    let front = string.substring(0, take);
+    return front + string + front;
+}
+
+console.log(front22("kitten"))
