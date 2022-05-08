c4c3bb0591f6b5583a55b562e20a321acb7b398d
Adrian Brown <adrian.r.brown90@gmail.com>
2022-05-07 18:51:15 -0700

    add refactor of leet code in3050 from java to javascript

--- js/codeup_warmups.js
+++ js/codeup_warmups.js
@@ -172,3 +172,16 @@ function close10 (num1, num2) {
 }
 
 console.log(close10(3, 1))
+console.log("//////////")
+
+//Given 2 int values, return true if they are both in the range 30..40 inclusive,
+// or they are both in the range 40..50 inclusive.
+
+function in3050 (a, b) {
+    if (a >= 30 && a <= 40 && b >= 30 && b <= 40) { return true; }
+    if (a >= 40 && a <= 50 && b >= 40 && b <= 50) { return true; }
+    return false;
+}
+
+console.log(in3050(35, 32))
+console.log(in3050(12, 25))
