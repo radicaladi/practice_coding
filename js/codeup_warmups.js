52af9e918d638fa031d05982144cd32b023a8ca5
Adrian Brown <adrian.r.brown90@gmail.com>
2022-05-08 19:53:30 -0700

    add leet code everyNth

--- js/codeup_warmups.js
+++ js/codeup_warmups.js
@@ -202,4 +202,15 @@ function endUp (string) {
 console.log(endUp("california"))
 console.log("//////////")
 
+//Given a non-empty string and an int N, return the string made starting with char 0, and then every Nth char of the string.
+// So if N is 3, use char 0, 3, 6, ... and so on. N is 1 or more.
 
+function everyNth (string, number) {
+    let result = "";
+    for (let i = 0; i < string.length; i = i + number) {
+        result = result + string.charAt(i);
+    }
+    return result;
+}
+
+console.log(everyNth("supercalifragilistic", 3))
