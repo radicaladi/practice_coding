07f9198436a15096fb0452b27a496805a0965973
Adrian Brown <adrian.r.brown90@gmail.com>
2022-05-30 16:47:58 -0700

    adding or35 function exercise

--- js/codeup_warmups.js
+++ js/codeup_warmups.js
@@ -369,3 +369,13 @@ function backAround(string) {
 
 console.log(backAround("luke"));
 console.log("//////////")
+
+// Return true if the given non-negative number is a multiple of 3 or a multiple of 5. Use the % "mod" operator
+
+function or35 (n) {
+    return n > 0 && n % 3 === 0 || n % 5 === 0;
+}
+
+console.log(or35(70));
+console.log(or35(1));
+console.log("//////////");
